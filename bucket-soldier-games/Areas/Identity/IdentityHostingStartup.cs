ce0710967e528b4c819df3e89eb165223419abfa
avozda <56001186+avozda@users.noreply.github.com>
2021-06-18 09:25:47 +0200

    Add files via upload

---
+++ bucket-soldier-games/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using bucket_soldier_games.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(bucket_soldier_games.Areas.Identity.IdentityHostingStartup))]
+namespace bucket_soldier_games.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
